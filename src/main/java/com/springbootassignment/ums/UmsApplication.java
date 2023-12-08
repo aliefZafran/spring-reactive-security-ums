bbdfeeb91a0f1efa235fcb51eb08279cf4a893c0
leppo <zafrancfa@gmail.com>
2023-12-08 21:15:48 +0800

    add flyway integrations

--- src/main/java/com/springbootassignment/ums/UmsApplication.java
+++ src/main/java/com/springbootassignment/ums/UmsApplication.java
@@ -15,16 +15,6 @@ import org.springframework.web.reactive.config.EnableWebFlux;
 @SpringBootApplication
 public class UmsApplication {
 
-	@Bean
-	ConnectionFactoryInitializer initializer(ConnectionFactory connectionFactory) {
-
-		ConnectionFactoryInitializer initializer = new ConnectionFactoryInitializer();
-		initializer.setConnectionFactory(connectionFactory);
-		initializer.setDatabasePopulator(new ResourceDatabasePopulator(new ClassPathResource("schema.sql")));
-
-		return initializer;
-	}
-
 	public static void main(String[] args) {
 		SpringApplication.run(UmsApplication.class, args);
 	}
